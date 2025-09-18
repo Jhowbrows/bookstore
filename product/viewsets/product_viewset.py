1b7ee89cef16d62f86565dd68354ad46667feb75
Jhonatan <jhowbrows321@gmail.com>
2025-09-18 18:37:51 -0300

    viewsets

---
+++ product/viewsets/product_viewset.py
@@ -0,0 +1,12 @@
+from rest_framework.viewsets import ModelViewSet
+
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+
+class ProductViewSet(ModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_querySet(self):
+        return Product.objects.all()
