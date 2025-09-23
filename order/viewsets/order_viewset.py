b54badcd44ec9f0cb2040a2bad11e5b657da4617
Jhonatan <jhowbrows321@gmail.com>
2025-09-23 16:49:19 -0300

    resolving merge conflict

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,5 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication,
+    TokenAuthentication,
+)
 from rest_framework.permissions import IsAuthenticated
 from order.models import Order
 from order.serializer import OrderSerializer
