f8611f3e9b4dff1e4d9bd6fa927a7f0260c6e87e
Jhonatan <jhowbrows321@gmail.com>
2025-09-23 16:53:29 -0300

    codefactor fixing test2

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from product.models import Product
-from product.serializers.product_serializer  import ProductSerializer
+from product.serializers.product_serializer import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
